00519b0150ca611a03e049d7ab60231163e1626b
Noor Adiana <nooradiana@localhost.localdomain>
2016-05-28 22:37:39 +0700

    Setting controllers

--- app/dependencies.php
+++ app/dependencies.php
@@ -2,6 +2,9 @@
 
 $container = $app->getContainer();
 
+// -----------------------------------------------------------------------------
+// Twig
+// -----------------------------------------------------------------------------
 $container['view'] = function ($container){
     $settings = $container->settings;
     $view = new Slim\Views\Twig($settings['view']['template_path'], $settings['view']['twig']);
@@ -11,4 +14,11 @@ $container['view'] = function ($container){
     ));
     $view->addExtension(new Twig_Extension_Debug());
     return $view;
+};
+
+// -----------------------------------------------------------------------------
+// Controller factories
+// -----------------------------------------------------------------------------
+$container['HomeController'] = function ($container) {
+    return new App\Controllers\HomeController($container->view);
 };
